aff69f4609828dc544e2eb9a0df3d5d6a41a3843
Tony Atkins <duhrer@gmail.com>
2015-10-22 10:25:50 +0200

    GPII-1325:  Updated all tests to use standard schema keys and fixed broken all-tests.js.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -1,6 +1,6 @@
 "use strict";
+require("./js/validate-server-tests");
 require("./js/validate-client-tests");
-require("./js/validate-common-tests");
 require("./js/validate-function-tests");
 
 require("./js/middleware-tests.js");
