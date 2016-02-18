eb7d8e9dbdf6414a9883306df0c3f77d4d22438f
Tony Atkins <tony@raisingthefloor.org>
2016-02-18 12:22:02 +0000

    GPII-1336: Updated `errorBinder` prototype to work with new validation error format.

--- src/js/client/errorBinder.js
+++ src/js/client/errorBinder.js
@@ -1,8 +1,74 @@
+/* globals fluid */
+(function () {
+    "use strict";
+    var gpii = fluid.registerNamespace("gpii");
+    fluid.registerNamespace("gpii.schemas.client.errorBinder");
+
+    gpii.schemas.client.errorBinder.displayErrors = function (that) {
+        // Get rid of any previous validation errors.
+        that.locate("validationError").remove();
+
+        // Step through the list of bindings and look for anything that matches the current validation errors.
+        fluid.each(that.options.errorBindings, function (value, key) {
+            var selector     = typeof value === "string" ? key   : value.selector;
+            var element      = that.locate(selector);
+            if (element) {
+                // TODO:  Make an expander to automatically convert paths in binder definitions to usable dataPath values.
+                var expectedPath = "." + (typeof value === "string" ? value : value.path);
+                fluid.each(that.model.fieldErrors, function (error) {
+                    if (error.dataPath === expectedPath) {
+                        // element, key, context
+                        that.renderer.before(element, that.options.templates.inlineError, error);
+                    }
+                });
+            }
+        });
+
+        // We have inserted new elements and need to fire an event so that interested parties can update their bindings.
+        that.events.onDomBind.fire(that);
+    };
+
+    fluid.defaults("gpii.schemas.client.errorBinder", {
+        gradeNames: ["fluid.component"],
+        errorBindings: "{that}.options.bindings",
+        validationErrorClass: "validationError",
+        selectors: {
+            "validationError": ".validationError"
+        },
+        model: {
+            fieldErrors: [
+                {
+                    "keyword": "required",
+                    "dataPath": ".username",
+                    "message": "Username is a required field."
+                },
+                {
+                    "keyword": "pattern",
+                    "dataPath": ".password",
+                    "message": "Passwords must be at least 8 characters long, and must contain at least one uppercase character, lowercase character, and special character or number."
+                }
+            ]
+        },
+        components: {
+            // This very much will not work unless you pass in template content yourself.
+            renderer: {
+                type: "gpii.templates.renderer"
+            }
+        },
+        modelListeners: {
+            fieldErrors: {
+                funcName: "gpii.schemas.client.errorBinder.displayErrors",
+                excludeSource: "init",
+                args: ["{that}"]
+            }
+        }
+    });
+
 /*
 
     These client side grades use model->view bindings like those used with `gpii.templates.binder` to associate
     validation errors reported by the validator with onscreen elements.  That "binding" structure looks something like:
-    
+
     ```
     bindings: {
         "key": {
@@ -21,88 +87,7 @@
     grade that performs all the necessary wiring is also included here.
 
  */
-
-/* global fluid, $ */
-(function () {
-    "use strict";
-    var gpii = fluid.registerNamespace("gpii");
-    fluid.registerNamespace("gpii.schemas.client.errorBinder");
-
-    gpii.schemas.client.errorBinder.displayErrors = function (that) {
-        // Get rid of any previous validation errors.
-        fluid.each(that.locate("validationError"), function (element) {
-            element.remove();
-        });
-
-        // Step through the list of bindings and look for anything that matches the current validation errors.
-        fluid.each(that.options.errorBindings, function (value, key) {
-            var path = typeof value === "string" ? value : value.path;
-            var selector = typeof value === "string" ? key : value.selector;
-
-            var validationErrors = fluid.get(that.model.validationErrors, path + ".validationErrors");
-            if (validationErrors) {
-                var element = that.locate(selector);
-                if (element) {
-                    var errors = fluid.makeArray(validationErrors);
-                    fluid.each(errors, function (error) {
-                        // element, key, context
-                        that.renderer.before(element, that.options.templates.inlineError, error);
-                    });
-                }
-            }
-        });
-
-        // We have inserted new elements and need to fire an event so that interested parties can update their bindings.
-        that.events.onDomBind.fire(that);
-    };
-
-    fluid.defaults("gpii.schemas.client.errorBinder", {
-        gradeNames: ["fluid.component"],
-        errorBindings: "{that}.options.bindings",
-        validationErrorClass: "validationError",
-        selectors: {
-            "validationError": ".validationError"
-        },
-        model: {
-            validationErrors: {
-                // TODO:  Improve the validator output and wire it in instead of the hard-coded data.
-                // TODO:  Test this with deep paths.
-                "username": {
-                    validationErrors: [
-                        {
-                            "keyword": "required",
-                            "dataPath": ".username",
-                            "message": "is a required property",
-                            "description": "Username is a required field."
-                        }
-                    ]
-                },
-                "password": {
-                    validationErrors: [
-                        {
-                            "keyword": "pattern",
-                            "dataPath": ".password",
-                            "message": "should match pattern \"[A-Z]+\"",
-                            "description": "Passwords must be at least 8 characters long, and must contain at least one uppercase character, lowercase character, and special character or number."
-                        }
-                    ]
-                }
-            }
-        },
-        components: {
-            // This very much will not work unless you pass in template content yourself.
-            renderer: {
-                type: "gpii.templates.renderer"
-            }
-        },
-        modelListeners: {
-            validationErrors: {
-                funcName: "gpii.schemas.client.errorBinder.displayErrors",
-                excludeSource: "init",
-                args: ["{that}"]
-            }
-        }
-    });
+/* global fluid */
 
     // TODO: revalidate using client-side validation when the model changes.
 
@@ -113,11 +98,11 @@
         gradeNames: ["gpii.schemas.client.errorBinder", "gpii.templates.templateFormControl"],
         rules: {
             successResponseToModel: {
-                validationErrors: { literalValue: null }
+                fieldErrors: { literalValue: null }
             },
             errorResponseToModel: {
                 errorMessage: "responseJSON.message",
-                validationErrors: "responseJSON.validationErrors"
+                fieldErrors:  "responseJSON.fieldErrors"
             },
             modelToRequestPayload: {
                 "": "notfound",
