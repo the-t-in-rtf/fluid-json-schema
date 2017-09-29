98af13e1262782a9908d8a7efe2d4abd67542b3e
Tony Atkins <tony@raisingthefloor.org>
2017-09-29 13:29:49 +0200

    NOJIRA: Reenabled parser failure tests to assist in troubleshooting issues further.

--- tests/js/server/index.js
+++ tests/js/server/index.js
@@ -5,7 +5,7 @@ require("./middleware-contentAware-tests");
 require("./middleware-requestAware-tests");
 require("./parser-tests");
 // TODO: Work with Antranig to figure out why the parser errors are not being trapped correctly.
-// require("./parser-update-failure-tests");
+require("./parser-update-failure-tests");
 require("./pointer-function-tests");
 require("./schema-inline-router-tests");
 require("./url-assembler-tests");
